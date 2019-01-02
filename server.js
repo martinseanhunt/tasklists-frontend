e2981c8a453cfb22735aab8bf7554fdd5a8e671b
unknown <martinseanhunt@gmail.com>
2019-01-02 16:04:58 +0000

    show node env

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+
+console.log(process.env.NODE_ENV)
  
 const {createServer} = require('http')
 app.prepare().then(() => {
